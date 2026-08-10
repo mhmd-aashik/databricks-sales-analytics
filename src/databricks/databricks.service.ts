1ea161c54a752a2885daa289fa3830d2d919692c
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-10 19:02:26 +0400

    Implement DatabricksService with connection testing functionality
    - Added testConnection method to DatabricksService for establishing a connection to Databricks.
    - Utilized DBSQLClient to execute a sample SQL query and fetch results.
    - Integrated ConfigService for environment variable management.

--- src/databricks/databricks.service.ts
+++ src/databricks/databricks.service.ts
@@ -1,4 +1,35 @@
+import { DBSQLClient } from '@databricks/sql';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class DatabricksService {}
+export class DatabricksService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async testConnection() {
+    const client = new DBSQLClient();
+
+    const connection = await client.connect({
+      host: this.configService.get('DATABRICKS_HOST')!,
+      path: this.configService.get('DATABRICKS_PATH')!,
+      token: this.configService.get('DATABRICKS_TOKEN')!,
+    });
+
+    const session = await connection.openSession();
+
+    const operation = await session.executeStatement(
+      'SELECT * FROM workspace.default.product_revenue ORDER BY revenue DESC',
+      {
+        runAsync: true,
+      },
+    );
+
+    const result = await operation.fetchAll();
+
+    await operation.close();
+    await session.close();
+    await connection.close();
+
+    return result;
+  }
+}
